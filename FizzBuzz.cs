ce757a3711058596fd489158aff0b2d0552104c6
Mark Joshua Tarcelo <markjoshuatarcelo@gmail.com>
2024-08-21 20:09:40 +0800

    Update FizzBuzz.cs

--- FizzBuzz.cs
+++ FizzBuzz.cs
@@ -9,3 +9,19 @@ for(int i = 1; i <= 100; i++){
         Console.WriteLine(i);
     }
 }
+
+// second way
+var map = new Dictionary<int, string>(){
+    {3, "Fizz"},
+    {5, "Buzz"},
+    {7, "Bazz"},
+};
+for(int i = 1; i <= 100; i++){
+    string output ="";
+    foreach(var key in map){
+        if(i % key.Key==0 ){
+            output += key.Value;
+        }
+    }
+    Console.WriteLine(string.IsNullOrEmpty(output) ? i.ToString() : output);
+}
